63336700a24bb4bf9b07e3446241c493b1299574
Madison <midunitz@ucdavis.edu>
2016-10-17 17:55:11 -0700

    deploy

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = Server.app()
 if (process.env.NODE_ENV !== 'production') {
 
   const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
+  // const webpackDevMiddleware = require('webpack-dev-middleware')
+  // const webpackHotMiddleware = require('webpack-hot-middleware')
   const config = require('./webpack.dev.config.js')
   const compiler = webpack(config)
 
