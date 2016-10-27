6e97d0172b2200a178c14a7c8ff70b351cc83c5e
Madison <midunitz@ucdavis.edu>
2016-10-27 15:01:40 -0700

    issues with component mounting

--- src/components/orderKit.js
+++ src/components/orderKit.js
@@ -6,13 +6,22 @@ class OrderKit extends Component {
   constructor(props){
     super(props);
     //initalize local state 
+    this.handleChangeKitty = this.handleChangeKitty.bind(this);
+    this.handleChangeDoggy = this.handleChangeDoggy.bind(this);
     this.state = {doggyKit: 0, kittyKit: 0, cost:0};
+
   }
+
+  componentDidMount(){}
   //event handler, when user selects a kit or tshirt update state/shopping cart with the new values
   handleChangeKitty(event){
+    
+
     //TODO pass in old state to update
     var doggyCost = this.state.doggyKit * 99;
-    var kittyCost = event.target.value * 99
+    console.log("event", this)
+    alert("cajknekjfndkjsn")
+    var kittyCost = event.target.value * 99 || 0
     var cost = kittyCost + doggyCost
     this.setState({kittyKit: event.target.value, cost: cost})
     console.log(this.state)
@@ -38,38 +47,39 @@ class OrderKit extends Component {
     return(
       <div>
       <form onsubmit={this.handleSubmit.bind(this)}>
-        <h1>Put a picture of the kitty kit here</h1>
-        <select onSelect={this.handleChangeKitty.bind(this)} className="kitty">
-          <optgroup value={this.state.kittyKit} label="Number of kitty kits">
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-            <option value=7>7</option>
-            <option value=8>8</option>
-            <option value=9>9</option>
-            <option value=10>10</option>
+        <h3>Put a picture of the kitty kit here</h3>
+        <select  value={this.state.kittyKit} className="kitty">
+          <optgroup onSelect={this.handleChangeKitty()}label="Number of kitty kits">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
           </optgroup>
         </select>
-        <h1>Put a picture of the doggy kit here</h1>
-        <select className="doggy">
-          <optgroup onSelect={this.handleChangeDoggy.bind(this)} value={this.state.doggyKit}  label="Number of doggy kits">
-            <option value=1>1</option>
-            <option value=2>2</option>
-            <option value=3>3</option>
-            <option value=4>4</option>
-            <option value=5>5</option>
-            <option value=6>6</option>
-            <option value=7>7</option>
-            <option value=8>8</option>
-            <option value=9>9</option>
-            <option value=10>10</option>
+        <h3>Put a picture of the doggy kit here</h3>
+        <select value={this.state.doggyKit}  className="doggy">
+          <optgroup onSelect={this.handleChangeDoggy()}  label="Number of doggy kits">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
           </optgroup>
         </select>
-        <button action="submit">Order kits></button>
+        <button action="submit">Order kits</button>
       </form>
+      <div>Total Cost = {this.state.cost}</div>
       </div>
     )
   }
