09445fd04f164ed88e161cbac4f82d9fe1d58492
Madison <midunitz@ucdavis.edu>
2016-11-04 13:47:54 -0700

    minify images for production

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -33,7 +33,35 @@ module.exports = {
       { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         loader: 'file'},
       { test: /\.css$/,
-        loaders: ['style', 'css']}
+        loaders: ['style', 'css']},
+      { test: /.*\.(gif|png|jpe?g|svg)$/i,
+        loaders: ['file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack'
+        ]
+      }
     ]
+  },
+
+   imageWebpackLoader: {
+    mozjpeg: {
+      quality: 65
+    },
+    pngquant:{
+      quality: "65-90",
+      speed: 4
+    },
+    svgo:{
+      plugins: [
+        {
+          removeViewBox: false
+        },
+        {
+          removeEmptyAttrs: false
+        }
+      ]
+    }
   }
 }
+
+
+
