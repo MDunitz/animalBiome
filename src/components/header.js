99ae32a3ee4039ed35ad069b5d240630e1670927
Madison <midunitz@ucdavis.edu>
2016-11-04 15:56:30 -0700

    header done

--- src/components/header.js
+++ src/components/header.js
@@ -8,19 +8,17 @@ class Header extends Component {
 
   render(){
     return(
-      <div>
-        <div className="headerDiv">
-          <div className="logoDiv">
-            <Link to="/" activeClassName="active">
-              <img className="logo" src={logo}/>
-            </Link>
-          </div>
+      <div className="headerDiv">
+        <div className="logoDiv">
+          <Link to="/" activeClassName="active">
+            <img className="logo" src={logo}/>
+          </Link>
           <ul className="header">
             <li><a href="https://catbiome.wordpress.com/" activeClassName="active">Blog</a></li>
             <li><Link to="/#howitworks" activeClassName="active">How It Works</Link></li>
             <li><a href='http://codap.concord.org/releases/latest/static/dg/en/cert/index.html#shared=15599' activeClassName="active">KittyBiome Data</a></li>
             <li><Link to="/#aboutus" activeClassName="active">About Us</Link></li>
-            <li><a href='https://animal-biome.myshopify.com/collections/all' activeClassName="active">Order a Kit</a></li>
+            <li><a href='https://animal-biome.myshopify.com/collections/sampling-kits' activeClassName="active">Order a Kit</a></li>
             <li><div className="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div></li>
           </ul>
         </div>
