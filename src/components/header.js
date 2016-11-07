831c54245eb98583c85c1998595f7816b3f1fd29
Madison <midunitz@ucdavis.edu>
2016-11-06 16:57:55 -0800

    hashlinks working

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { HashLink } from 'react-router-hash-scroll';
 import logo from "../../styles/assets/abLogo.png";
 
 
@@ -15,9 +16,9 @@ class Header extends Component {
           </Link>
           <ul className="header">
             <li><a href="https://catbiome.wordpress.com/" activeClassName="active">Blog</a></li>
-            <li><Link to="/#howitworks" activeClassName="active">How It Works</Link></li>
+            <li><HashLink to="#howitworks" activeClassName="active">How It Works</HashLink></li>
             <li><a href='http://codap.concord.org/releases/latest/static/dg/en/cert/index.html#shared=15599' activeClassName="active">KittyBiome Data</a></li>
-            <li><Link to="/#aboutus" activeClassName="active">About Us</Link></li>
+            <li><HashLink to="#aboutus" activeClassName="active">About Us</HashLink></li>
             <li><a href='https://animal-biome.myshopify.com/collections/sampling-kits' activeClassName="active">Order a Kit</a></li>
             <li><div className="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div></li>
           </ul>
--- src/components/home/animalKits.js
+++ src/components/home/animalKits.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import doggyKitPhoto from '../../../styles/assets/doggyLogo.png';
 import kittyKitPhoto from '../../../styles/assets/kittyLogo.png';
+import { HashLink } from 'react-router-hash-scroll';
 
 class AnimalKits extends Component {
   render(){
@@ -11,13 +12,13 @@ class AnimalKits extends Component {
           <img className="doggyKitPhoto" src={doggyKitPhoto}/>
           <h3><b>DoggyBiome</b><br/>Microbiome assessment<br/>for dogs</h3>
           <button className="button"><a href="https://animal-biome.myshopify.com/collections/frontpage/products/doggykit">Order Now</a></button>
-          <button className="button"><a href="https://animal-biome.myshopify.com">Learn More</a></button>
+          <button className="button"><HashLink to="#howitworks">Learn More</HashLink></button>
         </div>
         <div className="kittyKit col-md-4">
           <img className="kittyKitPhoto" src={kittyKitPhoto}/>
           <h3><b>KittyBiome</b><br/>Microbiome assessment<br/>for cats</h3>
           <button className="button"><a href="https://animal-biome.myshopify.com/products/kittykit">Order Now</a></button>
-          <button className="button"><a href="https://animal-biome.myshopify.com">Learn More</a></button>
+          <button className="button"><HashLink to="#howitworks">Learn More</HashLink></button>
         </div>
         
       </div>
